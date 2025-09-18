5b077b85c517d973796f42624a4d655408356903
Lewis Cook <lcook@FreeBSD.org>
2025-09-18 18:07:11 +0100

    fish: set YORHA* as global variables

--- fish/functions/__yorha_env_start.fish
+++ fish/functions/__yorha_env_start.fish
@@ -1,7 +1,7 @@
 function __yorha_env_start
     if test -d ~/dev/yorha
-        set -x YORHA_PROJECT_PATH ~/dev/yorha/
-        set -x YORHA_LIBEXEC_PATH $YORHA_PROJECT_PATH"libexec"
+        set -gx YORHA_PROJECT_PATH ~/dev/yorha/
+        set -gx YORHA_LIBEXEC_PATH $YORHA_PROJECT_PATH"libexec"
         set -x fish_user_paths $YORHA_PROJECT_PATH $fish_user_paths
     end
 end
