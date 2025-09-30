f78e1f181a3eab389641d87c6b39017f1e249f34
Lewis Cook <lcook@FreeBSD.org>
2025-09-30 23:24:54 +0100

    fish: update YORHA_* env variables

--- fish/functions/__yorha_env_start.fish
+++ fish/functions/__yorha_env_start.fish
@@ -1,7 +1,7 @@
 function __yorha_env_start
     if test -d ~/dev/yorha
-        set -gx YORHA_PROJECT_PATH ~/dev/yorha/
-        set -gx YORHA_LIBEXEC_PATH $YORHA_PROJECT_PATH"libexec"
-        set -x fish_user_paths $YORHA_PROJECT_PATH $fish_user_paths
+        set -gx YORHA_BASE_PATH ~/dev/yorha/
+        set -gx YORHA_LIBEXEC_PATH $YORHA_BASE_PATH"libexec"
+        set -x fish_user_paths $YORHA_BASE_PATH $fish_user_paths
     end
 end
