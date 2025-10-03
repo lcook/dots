0a2d6bbf7d81c9031383832e55a35e44f2e27c71
Lewis Cook <lcook@FreeBSD.org>
2025-10-03 19:52:16 +0100

    fish: unset YORHA_* properly

--- fish/functions/__yorha_env_start.fish
+++ fish/functions/__yorha_env_start.fish
@@ -3,5 +3,8 @@ function __yorha_env_start
         set -gx YORHA_BASE_PATH ~/dev/yorha/
         set -gx YORHA_LIBEXEC_PATH $YORHA_BASE_PATH"libexec"
         set -x fish_user_paths $YORHA_BASE_PATH $fish_user_paths
+    else
+        set -e YORHA_BASE_PATH
+        set -e YORHA_LIBEXEC_PATH
     end
 end
