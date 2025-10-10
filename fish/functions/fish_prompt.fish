33631bb56615738f89169b1c41f2eeca75ab4149
Lewis Cook <lcook@FreeBSD.org>
2025-10-10 20:28:18 +0100

    fish: remove background colors from prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,5 +3,5 @@ function fish_prompt
         echo -n "* "
     end
 
-    echo -e (set_color $random_color)\u2589(set_color -r $random_color) $(hostname) (set_color normal) $(prompt_pwd -D2) (fish_git_prompt)(set_color normal) \u1409" "
+    echo -e (set_color $random_color)$(hostname) (set_color normal)$(prompt_pwd -D2) (fish_git_prompt) \u03bb" "
 end
